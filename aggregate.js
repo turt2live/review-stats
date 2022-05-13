b41d860eec9844d506f2f369421210d87b25b7fa
Travis Ralston <travpc@gmail.com>
2022-05-13 00:22:07 -0600

    Add an aggregation script

---
+++ aggregate.js
@@ -0,0 +1,150 @@
+const analyze = require("./lib/analyze");
+const findWeekStart = require("./lib/findWeekStart");
+const getPrs = require("./lib/getPrs");
+const moment = require("moment");
+const path = require("path");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+
+async function aggregateTeam(teamName, repoRefs) {
+    console.log(`Generating aggregated stats for ${teamName} in repos:`, repoRefs);
+
+    const allStats = [];
+    for (const ref of repoRefs) {
+        const [orgName, repoName] = ref.split('/');
+        const { prs, hasCommunity } = await getPrs(orgName, repoName, teamName);
+        const stats = await analyze(prs, hasCommunity, orgName, repoName, teamName);
+        allStats.push({
+            orgName,
+            repoName,
+            teamName,
+            prs,
+            hasCommunity,
+            ...stats,
+        });
+    }
+
+    const thisWeek = findWeekStart(moment()).format('YYYY-MM-DD');
+    const lastWeek = findWeekStart(moment()).add(-1, 'week').format('YYYY-MM-DD');
+
+    const template = {
+        queueStart: 0,
+        queueEnd: 0,
+        queueMax: 0,
+        queueVolume: 0,
+        lagMin: Number.MAX_SAFE_INTEGER,
+        lagMax: Number.MIN_SAFE_INTEGER,
+        leastReviewed: {days: 0, prs: [/* "https://..." */]},
+    };
+
+    const statsLastWeek = {
+        core: JSON.parse(JSON.stringify(template)),
+        community: JSON.parse(JSON.stringify(template)),
+    };
+    const statsThisWeek = {
+        core: JSON.parse(JSON.stringify(template)),
+        community: JSON.parse(JSON.stringify(template)),
+    };
+
+    for (const record of allStats) {
+        const coreQueueLastWeek = record.coreByWeek[lastWeek]["queueSize"];
+        const communityQueueLastWeek = record.communityByWeek[lastWeek]["queueSize"];
+        const coreQueueThisWeek = record.coreByWeek[thisWeek]["queueSize"];
+        const communityQueueThisWeek = record.communityByWeek[thisWeek]["queueSize"];
+
+        mapQueueToObj(parseQueue(coreQueueLastWeek), statsLastWeek.core);
+        mapQueueToObj(parseQueue(communityQueueLastWeek), statsLastWeek.community);
+        mapQueueToObj(parseQueue(coreQueueThisWeek), statsThisWeek.core);
+        mapQueueToObj(parseQueue(communityQueueThisWeek), statsThisWeek.community);
+
+        statsLastWeek.core.queueVolume += record.coreByWeek[lastWeek]["openToReviewRequest"].length;
+        statsLastWeek.community.queueVolume += record.communityByWeek[lastWeek]["openToReviewRequest"].length;
+        statsThisWeek.core.queueVolume += record.coreByWeek[thisWeek]["openToReviewRequest"].length;
+        statsThisWeek.community.queueVolume += record.communityByWeek[thisWeek]["openToReviewRequest"].length;
+
+        statsLastWeek.core.lagMin = Math.min(statsLastWeek.core.lagMin, ...record.coreByWeek[lastWeek]["reviewRequestToFirst"]);
+        statsLastWeek.community.lagMin = Math.min(statsLastWeek.community.lagMin, ...record.communityByWeek[lastWeek]["reviewRequestToFirst"]);
+        statsThisWeek.core.lagMin = Math.min(statsThisWeek.core.lagMin, ...record.coreByWeek[thisWeek]["reviewRequestToFirst"]);
+        statsThisWeek.community.lagMin = Math.min(statsThisWeek.community.lagMin, ...record.communityByWeek[thisWeek]["reviewRequestToFirst"]);
+
+        statsLastWeek.core.lagMax = Math.max(statsLastWeek.core.lagMax, ...record.coreByWeek[lastWeek]["reviewRequestToFirst"]);
+        statsLastWeek.community.lagMax = Math.max(statsLastWeek.community.lagMax, ...record.communityByWeek[lastWeek]["reviewRequestToFirst"]);
+        statsThisWeek.core.lagMax = Math.max(statsThisWeek.core.lagMax, ...record.coreByWeek[thisWeek]["reviewRequestToFirst"]);
+        statsThisWeek.community.lagMax = Math.max(statsThisWeek.community.lagMax, ...record.communityByWeek[thisWeek]["reviewRequestToFirst"]);
+
+        mapLeastReviewedToObj(record.leastReviewCoreByWeek[lastWeek], statsLastWeek.core.leastReviewed);
+        mapLeastReviewedToObj(record.leastReviewCommunityByWeek[lastWeek], statsLastWeek.community.leastReviewed);
+        mapLeastReviewedToObj(record.leastReviewCoreByWeek[thisWeek], statsThisWeek.core.leastReviewed);
+        mapLeastReviewedToObj(record.leastReviewCommunityByWeek[thisWeek], statsThisWeek.community.leastReviewed);
+    }
+
+    return { statsLastWeek, statsThisWeek };
+}
+
+function mapLeastReviewedToObj(leastRev, obj) {
+    if (leastRev.days > obj.days) {
+        obj.prs = leastRev.prs;
+    } else if (leastRev.days === obj.days) {
+        obj.prs = [...obj.prs, ...leastRev.prs];
+    }
+}
+
+function mapQueueToObj(stats, obj) {
+    obj.queueStart += stats.open;
+    obj.queueEnd += stats.close;
+    obj.queueMax += stats.max;
+}
+
+function parseQueue(week) {
+    let open = 0;
+    let close = 0;
+    let max = Number.MIN_VALUE;
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < 7; i++) {
+        let c = 0;
+        for (const day of week) {
+            const v = day[i];
+            if (v === null) continue;
+            if (v >= 0) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+        if (i === 0) open = c;
+        if (i === 6) close = c;
+        max = Math.max(max, c);
+        min = Math.min(min, c);
+    }
+    return {open, close, min, max};
+}
+
+function renderSection(stats) {
+    const leastReviewedList = stats.leastReviewed.prs.map(pr => {
+        const shorthand = pr.replace(/https:\/\/github.com\/[^\/]*\//, '').replace('/pull/', '#');
+        return `<li><a href="${pr}">${shorthand}</a></li>`;
+    })
+
+    return ""
+        + `<li>Queue size: Started at ${stats.queueStart}, ended at ${stats.queueEnd}. Max ${stats.queueMax}. Volume ${stats.queueVolume}.</li>`
+        + `<li>Review lag: ${stats.queueVolume <= 0 ? "No PRs to review." : `${stats.lagMin} - ${stats.lagMax} calendar days`}</li>`
+        + `<li>Least reviewed PRs: ${stats.leastReviewed.prs.length}-way tie at ${stats.leastReviewed.days} calendar days.<ul>${leastReviewedList.join('')}</ul></li>`;
+}
+
+(async function() {
+    const repos = process.argv.slice(3);
+    const teamName = process.argv[2];
+    const { statsLastWeek, statsThisWeek } = await aggregateTeam(teamName, repos);
+
+    const htmlOutDir = "html";
+    const outDir = path.join(htmlOutDir, "_weeklysync", teamName);
+    mkdirp.sync(outDir);
+
+    const output = ""
+        + `<ul><li>Core team PRs:<ul><li>Last week (finalized):<ul>${renderSection(statsLastWeek.core)}</ul></li>`
+        + `<li>This week (so far):<ul>${renderSection(statsThisWeek.core)}</ul></li></ul>`
+        + `<li>Community PRs:<ul><li>Last week (finalized):<ul>${renderSection(statsLastWeek.community)}</ul></li>`
+        + `<li>This week (so far):<ul>${renderSection(statsThisWeek.community)}</ul></li></ul>`;
+
+    fs.writeFileSync(path.join(outDir, "summary.html"), output, "utf-8");
+})();
--- gen.js
+++ gen.js
@@ -1,242 +1,25 @@
-const {graphql} = require("@octokit/graphql");
-const repoPrs = require("./queries/repo-prs");
 const {Liquid} = require("liquidjs");
+const mkdirp = require("mkdirp");
 const path = require("path");
 const fs = require("fs");
-const mkdirp = require("mkdirp");
 const moment = require("moment");
-
-let auth = {token: process.env['GH_PAT']};
-if (!auth.token) {
-    auth = require("./auth.json");
-}
+const getPrs = require('./lib/getPrs');
+const analyze = require("./lib/analyze");
 
 module.exports = async function(orgName, repoName, teamName) {
     console.log("Generating for: ", {orgName, repoName, teamName});
-    const tmpFile = path.join('tmp', `tmp.${orgName}.${repoName}.json`);
+    const { prs, hasCommunity } = await getPrs(orgName, repoName, teamName);
 
-    const prs = [];
-    let hasCommunity = true;
-    if (fs.existsSync(tmpFile)) {
-        const data = JSON.parse(fs.readFileSync(tmpFile));
-        prs.push(...data['prs']);
-        hasCommunity = data.hasCommunity !== false;
-    } else {
-        let lastId = null;
-        while (true) {
-            console.log(`Getting prs starting at ${lastId}`);
-            const {repository} = await graphql(repoPrs(orgName, repoName, lastId), {
-                headers: {
-                    authorization: `token ${auth.token}`,
-                },
-            });
-            const nodes = repository['pullRequests']['edges'];
-            prs.push(...(nodes.map(e => e['node'])));
-            if (nodes.length === 0) break;
-            lastId = nodes[nodes.length - 1]['cursor'];
-            if (repository.isPrivate) {
-                hasCommunity = false;
-            }
-        }
-    }
-
-    fs.writeFileSync(tmpFile, JSON.stringify({prs, hasCommunity}, null, 4));
-
-    // Metrics:
-    // Queue size by week
-    // Time from Open -> Review Request
-    // Time from Review Request -> First Review
-    // Time from Review Request -> Approving Review
-
-    const metricsByWeek = { // "week" starts on the Sunday and is the day the first review request is made
-        // [YYYY-MM-DD]: {
-        //     "queueSize": [[change in days]],
-        //     "openToReviewRequest": [# days],
-        //     "reviewRequestToFirst": [# days],
-        //     "reviewRequestToApproved": [# days],
-        // }
-    };
-    const coreByWeek = {};
-    const communityByWeek = {};
-    const incompleteByWeek = {};
-    const coreIncompleteByWeek = {};
-    const communityIncompleteByWeek = {};
-
-    const leastReviewCoreByWeek = {};
-    const leastReviewCommunityByWeek = {};
-
-    const ghFormat = "YYYY-MM-DDTHH:mm:ssZ"
-
-    function findWeekStart(date) { // moment
-        return moment(date).subtract(date.day(), 'days'); // Sunday is zero, so find the last day of week
-    }
-
-    for (const pr of prs) {
-        const createdDate = moment(pr['createdAt'], ghFormat);
-        const events = pr['timelineItems']['edges'].map(e => e['node']);
-
-        const reviewRequests = events.filter(e => e['__typename'] === 'ReviewRequestedEvent' && e['requestedReviewer'] && e['requestedReviewer']['name'] === teamName);
-        const delistedEvents = [
-            ...events.filter(e => e['__typename'] === 'PullRequestReview' && e['onBehalfOf'] && e['onBehalfOf']['edges'] && e['onBehalfOf']['edges'].map(d => d['node']).filter(n => !!n && n['name'] === teamName)),
-            ...events.filter(e => e['__typename'] === 'ReviewRequestRemovedEvent' && e['requestedReviewer'] && e['requestedReviewer']['name'] === teamName),
-            ...events.filter(e => e['__typename'] === 'ClosedEvent' || e['__typename'] === 'MergedEvent'),
-        ];
-        delistedEvents.sort((a, b) => {
-            const aDate = moment.utc(a['createdAt'], ghFormat);
-            const bDate = moment.utc(b['createdAt'], ghFormat);
-            return aDate.diff(bDate, 'days');
-        });
-
-        for (const req of reviewRequests) {
-            const reqDate = moment.utc(req['createdAt'], ghFormat);
-            const nextRemove = delistedEvents.find(e => reqDate.isSameOrBefore(moment.utc(e['createdAt'], ghFormat)));
-            const nextRemoveDate = nextRemove ? moment.utc(nextRemove['createdAt'], ghFormat) : moment.utc();
-
-            let mark = reqDate.clone();
-            const weekArrays = [[]];
-            let currentWeek = weekArrays[0];
-            let currentWeekLabel = findWeekStart(mark).format('YYYY-MM-DD');
-            do {
-                const week = findWeekStart(mark);
-                const dayIdx = mark.diff(week, 'days');
-
-                if (currentWeekLabel !== week.format('YYYY-MM-DD')) {
-                    while (currentWeek.length < 7) {
-                        currentWeek.push(null);
-                    }
-                    weekArrays.push([]);
-                    currentWeek = weekArrays[weekArrays.length - 1];
-                    currentWeekLabel = week.format('YYYY-MM-DD');
-                }
-
-                while (currentWeek.length < dayIdx) {
-                    currentWeek.push(null);
-                }
-
-                const num = mark.diff(nextRemoveDate, 'days') === 0 && mark.diff(reqDate, 'days') === 0
-                    ? 0
-                    : (mark.diff(nextRemoveDate, 'days') === 0 ? -1 : 1);
-                currentWeek.push(num);
-            } while(mark.add(1, 'days').diff(nextRemoveDate, 'days') < 0);
-
-            while (currentWeek.length < 7) {
-                currentWeek.push(null);
-            }
-
-            mark = findWeekStart(reqDate).clone();
-            const teamMetrics = (pr["authorAssociation"] === 'MEMBER' || !hasCommunity) ? coreByWeek : communityByWeek;
-            for (const week of weekArrays) {
-                const key = mark.format('YYYY-MM-DD');
-                if (!metricsByWeek[key]) {
-                    metricsByWeek[key] = {
-                        openToReviewRequest: [],
-                        reviewRequestToFirst: [],
-                        reviewRequestToApproved: [],
-                        queueSize: [],
-                    };
-                }
-                if (!teamMetrics[key]) {
-                    teamMetrics[key] = {
-                        openToReviewRequest: [],
-                        reviewRequestToFirst: [],
-                        reviewRequestToApproved: [],
-                        queueSize: [],
-                    };
-                }
-                metricsByWeek[key].queueSize.push(week);
-                teamMetrics[key].queueSize.push(week);
-                mark = mark.add(1, 'week');
-            }
-        }
-
-        const reviewRequest = events.find(e => e['__typename'] === 'ReviewRequestedEvent' && e['requestedReviewer'] && e['requestedReviewer']['name'] === teamName);
-        let firstReview = events.find(e => e['__typename'] === 'PullRequestReview' && e['onBehalfOf'] && e['onBehalfOf']['edges'] && e['onBehalfOf']['edges'].map(d => d['node']).filter(n => !!n).find(n => n['name'] === teamName));
-        let approvingReview = events.find(e => e['__typename'] === 'PullRequestReview' && e['state'] === 'APPROVED' && e['onBehalfOf'] && e['onBehalfOf']['edges'] && e['onBehalfOf']['edges'].map(d => d['node']).filter(n => !!n).find(n => n['name'] === teamName));
-        const lastRemoved = events.filter(e => e['__typename'] === 'ReviewRequestRemovedEvent' && e['requestedReviewer'] && e['requestedReviewer']['name'] === teamName).sort((a, b) => (a < b) ? 1 : ((a === b) ? 0 : -1))[0];
-
-        if (!reviewRequest) continue;
-        let incomplete = !firstReview || !approvingReview;
-
-        // Consider last removed review request as the (incomplete) review date if needed
-        if (!approvingReview && lastRemoved && lastRemoved['createdAt'] > (firstReview ? firstReview['createdAt'] : '')) {
-            approvingReview = lastRemoved;
-            if (!firstReview) firstReview = lastRemoved;
-        }
-
-        // Count merged without review as reviewed on the merge date
-        if (incomplete && pr['state'] === 'MERGED') {
-            if (!firstReview) firstReview = {createdAt: pr['closedAt']};
-            if (!approvingReview) approvingReview = {createdAt: pr['closedAt']};
-            incomplete = false;
-        }
-
-        const requestDate = moment.utc(reviewRequest['createdAt'], ghFormat);
-        const firstDate = (incomplete && !firstReview) ? moment.utc() : moment.utc(firstReview['createdAt'], ghFormat);
-        const approvedDate = (incomplete && !approvingReview) ? moment.utc() : moment.utc(approvingReview['createdAt'], ghFormat);
-
-        const week = findWeekStart(requestDate);
-        const key = week.format('YYYY-MM-DD');
-        const keyLeastReview = findWeekStart(requestDate).format('YYYY-MM-DD');
-
-        const mainMetrics = incomplete ? incompleteByWeek : metricsByWeek;
-        const groupedByWeek = (pr["authorAssociation"] === 'MEMBER' || !hasCommunity)
-            ? (incomplete ? coreIncompleteByWeek : coreByWeek)
-            : (incomplete ? communityIncompleteByWeek : communityByWeek);
-        const leastReviewByWeek = (pr["authorAssociation"] === 'MEMBER' || !hasCommunity)
-            ? leastReviewCoreByWeek
-            : leastReviewCommunityByWeek;
-        if (!mainMetrics[key]) {
-            mainMetrics[key] = {
-                openToReviewRequest: [],
-                reviewRequestToFirst: [],
-                reviewRequestToApproved: [],
-                queueSize: [],
-            };
-        }
-        if (!groupedByWeek[key]) {
-            groupedByWeek[key] = {
-                openToReviewRequest: [],
-                reviewRequestToFirst: [],
-                reviewRequestToApproved: [],
-                queueSize: [],
-            };
-        }
-        if (!leastReviewByWeek[keyLeastReview]) {
-            leastReviewByWeek[keyLeastReview] = {
-                prs: [],
-                days: -1,
-            };
-        }
-
-        mainMetrics[key].openToReviewRequest.push(requestDate.diff(createdDate, 'days'));
-        mainMetrics[key].reviewRequestToFirst.push(firstDate.diff(requestDate, 'days'));
-        mainMetrics[key].reviewRequestToApproved.push(approvedDate.diff(requestDate, 'days'));
-
-        groupedByWeek[key].openToReviewRequest.push(requestDate.diff(createdDate, 'days'));
-        groupedByWeek[key].reviewRequestToFirst.push(firstDate.diff(requestDate, 'days'));
-        groupedByWeek[key].reviewRequestToApproved.push(approvedDate.diff(requestDate, 'days'));
-
-        if (pr['state'] !== 'CLOSED') {
-            const daysStuck = firstDate.diff(requestDate, 'days');
-            if (leastReviewByWeek[keyLeastReview].days < daysStuck) {
-                leastReviewByWeek[keyLeastReview].prs = [pr['url']];
-                leastReviewByWeek[keyLeastReview].days = daysStuck;
-            } else if (leastReviewByWeek[keyLeastReview].days === daysStuck) {
-                leastReviewByWeek[keyLeastReview].prs.push(pr['url']);
-            }
-        }
-    }
-
-    fs.writeFileSync(path.join('tmp', `byweek.${orgName}.${repoName}.json`), JSON.stringify(metricsByWeek, null, 4));
-    fs.writeFileSync(path.join('tmp', `byweek.core.${orgName}.${repoName}.json`), JSON.stringify(coreByWeek, null, 4));
-    fs.writeFileSync(path.join('tmp', `byweek.community.${orgName}.${repoName}.json`), JSON.stringify(communityByWeek, null, 4));
-
-    fs.writeFileSync(path.join('tmp', `byweek.incomplete.${orgName}.${repoName}.json`), JSON.stringify(incompleteByWeek, null, 4));
-    fs.writeFileSync(path.join('tmp', `byweek.incomplete.core.${orgName}.${repoName}.json`), JSON.stringify(coreIncompleteByWeek, null, 4));
-    fs.writeFileSync(path.join('tmp', `byweek.incomplete.community.${orgName}.${repoName}.json`), JSON.stringify(communityIncompleteByWeek, null, 4));
-
-    fs.writeFileSync(path.join('tmp', `byweek.core.leastreview.${orgName}.${repoName}.json`), JSON.stringify(leastReviewCoreByWeek, null, 4));
-    fs.writeFileSync(path.join('tmp', `byweek.community.leastreview.${orgName}.${repoName}.json`), JSON.stringify(leastReviewCommunityByWeek, null, 4));
+    const {
+        metricsByWeek,
+        coreByWeek,
+        communityByWeek,
+        incompleteByWeek,
+        coreIncompleteByWeek,
+        communityIncompleteByWeek,
+        leastReviewCoreByWeek,
+        leastReviewCommunityByWeek,
+    } = await analyze(prs, hasCommunity, orgName, repoName, teamName);
 
     const engine = new Liquid({root: './templates'});
     const htmlOutDir = "html";
